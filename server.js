fae6a745a9e7126eceff801a8f13b35f322cae1a
jeremy <jeremyfroment@yahoo.fr>
2018-01-21 21:50:24 +0100

    server

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
-var app = express.createServer();
+var app = express();
 
-app.use(express.staticProvider(__dirname));
+app.use(express.static(__dirname));
 
 app.get('/', function(req, res) {
     res.render('index.html');
