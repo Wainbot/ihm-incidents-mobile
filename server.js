882c59ca1a817485a038d78b141764adeeeea59d
jeremy <jeremyfroment@yahoo.fr>
2018-01-21 22:03:22 +0100

    server

--- server.js
+++ server.js
@@ -2,9 +2,10 @@ var express = require('express');
 var app = express();
 
 app.use(express.static(__dirname));
+app.set('port', process.env.PORT || 3000);
 
 app.get('/', function(req, res) {
     res.render('index.html');
 });
 
-app.listen(80);
+app.listen(app.get('port'));
